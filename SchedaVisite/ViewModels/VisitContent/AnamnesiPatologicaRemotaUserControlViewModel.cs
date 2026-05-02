051c5e3d26f9ab51f9602cc07a4c2368999723fd
Francesco Milano <milano.francesco@protonmail.com>
2026-05-02 09:34:55 +0200

    Added button for inference check

--- SchedaVisite/ViewModels/VisitContent/AnamnesiPatologicaRemotaUserControlViewModel.cs
+++ SchedaVisite/ViewModels/VisitContent/AnamnesiPatologicaRemotaUserControlViewModel.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using CommunityToolkit.Mvvm.ComponentModel;
 using SchedaVisite.Models;
 using SchedaVisite.Models.enums.anamnesipatologicaremota;
 
 namespace SchedaVisite.ViewModels.VisitContent;
 
-public class AnamnesiPatologicaRemotaUserControlViewModel: ViewModelBase
+public partial class AnamnesiPatologicaRemotaUserControlViewModel: ObservableObject
 {
     //CONSTRUCTORS
     public AnamnesiPatologicaRemotaUserControlViewModel(Visit currentVisit, Patient currentPatient)
@@ -18,4 +20,11 @@ public class AnamnesiPatologicaRemotaUserControlViewModel: ViewModelBase
     
     public IEnumerable<string> AmyloidosisTypesValues => AmyloidosisType.getAllAmyloidosisTypes();
     public IEnumerable<string> DementiaTypesValues => DementiaType.getAllDementiaTypes();
+
+    [ObservableProperty] private string _columnBDescription;
+
+    public void InferColumnBValues()
+    {
+        Console.WriteLine(ColumnBDescription);
+    }
 }
